f4d11a3fceb42641d0007daf16ce208a15f2ee50
Joakim Teixeira <setokim1@hotmail.com>
2025-06-15 12:38:49 +0100

    Create header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  standalone: true,
+  imports: [HeaderComponent],
   styleUrl: './app.scss',
 })
 export class App {
